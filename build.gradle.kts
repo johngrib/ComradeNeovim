5fa167d58fd75a0c4a94c62abeabc2326636680b
JohnGrib <johngrib82@gmail.com>
2023-03-30 21:34:50 +0900

    Version 0.1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "org.beeender"
-version = "0.1.3-SNAPSHOT"
+version = "0.1.4-SNAPSHOT"
 
 buildscript {
     repositories { mavenCentral() }
